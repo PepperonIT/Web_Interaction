ecd07c09f1fc08791dc51cb07ecdb91859688e0f
erik99s <erisre-8@student.ltu.se>
2022-10-05 15:07:40 +0200

    setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from naoqi import ALProxy
+
+
+def main():
+    # session = qi.Session()
+    tts = ALProxy("ALTextToSpeech", "130.240.238.32", 9559)
+
+    
+    #tts.say("Hello there")
+    tts.say("Hello there")
+
+main()
