dafe96a2d193c1fafe4a378b765c7544846f9152
erik99s <erisre-8@student.ltu.se>
2022-10-11 14:37:08 +0200

    added interactions folder

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from naoqi import ALProxy
+from PepperConnection import PepperConnection
+#from interactions.wikipedia import Wikipedia
+import os
 
 
 def main():
     # session = qi.Session()
-    tts = ALProxy("ALTextToSpeech", "130.240.238.32", 9559)
+    conn = PepperConnection("130.240.238.32", 9559, "nao", os.getenv("password")) 
+    conn.connect()   
+    
+    tts = conn.get_speech_service()
 
     tts.say("Hello there")
+    
+    #Wikipedia.ask_wikipedia()
     
 
 main()
