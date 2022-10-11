ca77b7a23a70224fe57e2eeafeabb8c7497e0ff8
erik99s <erisre-8@student.ltu.se>
2022-10-11 12:48:02 +0200

    added pepper connection

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def main():
     # session = qi.Session()
     tts = ALProxy("ALTextToSpeech", "130.240.238.32", 9559)
 
-    
-    #tts.say("Hello there")
     tts.say("Hello there")
+    
 
 main()
