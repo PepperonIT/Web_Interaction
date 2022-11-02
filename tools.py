084f2fb6a0fd0495d1700a30e10c3a42c8bba5e5
oscarlundberg95 <57449935+oscarlundberg95@users.noreply.github.com>
2022-11-02 12:53:13 +0000

    Update tools.py
    working for image, no longer implementing google search

--- tools.py
+++ tools.py
@@ -1,33 +1,20 @@
 import wikipedia
-import search_google.api
 
 
 def get_knowledge_wikipedia(term):
     #stuff
     # wikipedia.set_lang("sv")
+    # wikipedia.set_lang("en")
     summary = wikipedia.summary(term, sentences=2)
     return summary
 
-def get_knowledge_google(term):
-    # Define buildargs for cse api
-    s2ttest = 'cat'
-    searchterm = ''
-    searchterm = str(term)
-    buildargs = {
-    'serviceName': 'customsearch',
-    'version': 'v1',
-    'developerKey' : 'AIzaSyCyHJ_eKNSy6kSXvQafM3ExsKEf2Fuhe1M'
-    }
+def get_image_wikipedia(term):
+    page = wikipedia.page(term)
+    wikiimage = page.images[0]
+    print("[INFO]: wikiimage: " + wikiimage)
+    return wikiimage
 
-    # Define cseargs for search
-    cseargs = {
-    'q': s2ttest, #searchterm
-    'cx': '8144907c10b824ec2',
-    'num': 1
-    }
-    results = search_google.api.results(buildargs, cseargs)
-    results.preview()
-    print("[INFO]: results:" + results)
-    links = results.links
-    print("[INFO]: links:" + links)
-    return links
+
+# def get_knowledge_google(term):
+    # Define buildargs for cse api
+
