224322bb58aa783f4203cf6eff5455416e33f324
oscarlundberg95 <oscarlundberg95@hotmail.se>
2022-11-03 15:57:56 +0000

    Swedish working, pylint implemented

--- robot.py
+++ robot.py
@@ -1,17 +1,20 @@
+"""
+Robot module
+"""
+import time
 import qi
 import speech_recognition
-import playsound
-import time
 import paramiko
-from cryptography.hazmat.backends import default_backend
 from scp import SCPClient
+import requests
 import tools
 
 
-class Robot:
 
+class Robot:# pylint: disable=too-many-instance-attributes, old-style-class
+    """class robot"""
     def __init__(self, ip_address, port=9559):
-        #speech, ip etc
+        """ip, port"""
         self.session = qi.Session()
         self.session.connect("tcp://" + ip_address + ":" + str(port))
 
@@ -21,7 +24,8 @@ class Robot:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.load_system_host_keys()
-        ssh.connect(hostname=self.ip_address, username="nao", password="FBLovesLMS2019", allow_agent=False)
+        ssh.connect(hostname=self.ip_address, username="nao",
+                    password="FBLovesLMS2019", allow_agent=False)
         self.scp = SCPClient(ssh.get_transport())
 
         self.tts_service = self.session.service("ALAnimatedSpeech")
@@ -37,41 +41,47 @@ class Robot:
         self.led_service = self.session.service("ALLeds")
         self.tablet_service = self.session.service("ALTabletService")
 
-        self.recognizer = speech_recognition.Recognizer()
+        self.recognizer = speech_recognition.Recognizer() #ENGLISH QUICK TRANSCRIBING
 
-        print("[INFO]: Robot is initialized at " + ip_address + ":" + port)
-        self.tablet_service.preLoadImage("https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
+        print("[INFO]: Robot is initialized at " + ip_address + ":" + port)# pylint: disable=superfluous-parens
+        self.tablet_service.preLoadImage(
+            "https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
 
     def blink_eyes(self, rgb):
+        """rbg"""
         self.led_service.fadeRGB('AllLeds', rgb[0], rgb[1], rgb[2], 1.0)
 
     def turn_off_leds(self):
+        """."""
         self.blink_eyes([0, 0, 0])
 
     def download_file(self, file_name):
+        """file_name"""
         self.scp.get(file_name, local_path="/tmp/")
-        print("[INFO]: File " + file_name + " downloaded")
+        print("[INFO]: File "+ file_name + " downloaded")# pylint: disable=superfluous-parens
         self.scp.close()
 
 
     def listen(self):
+        """listen"""
         self.speech_service.setAudioExpression(False)
         self.speech_service.setVisualExpression(False)
         self.audio_recorder.stopMicrophonesRecording()
-        print("[INFO]: Speech recognition is in progress. Say something.")
+        print("[INFO]: Speech recognition is in progress. Say something.")# pylint: disable=superfluous-parens
         while True:
-            print(self.memory_service.getData("ALSpeechRecognition/Status"))
+            print(self.memory_service.getData("ALSpeechRecognition/Status"))# pylint: disable=superfluous-parens
             # if self.memory_service.getData("ALSpeechRecognition/Status") == "SpeechDetected":
-            self.audio_recorder.startMicrophonesRecording("/home/nao/speech.wav", "wav", 48000, (0, 0, 1, 0))
-            print("[INFO]: Robot is listening to you")
+            self.audio_recorder.startMicrophonesRecording(
+                "/home/nao/speech.wav", "wav", 48000, (0, 0, 1, 0))
+            print("[INFO]: Robot is listening to you")# pylint: disable=superfluous-parens
             self.blink_eyes([255, 255, 0])
             break
 
         while True:
             time.sleep(2)
-            # if self.memory_service.getData("ALSpeechRecognition/Status") == "EndOfProcess":
+            # if self.memory_service.getData("SpeechDetected") == False:
             self.audio_recorder.stopMicrophonesRecording()
-            print("[INFO]: Robot is not listening to you")
+            print("[INFO]: Robot is not listening to you")# pylint: disable=superfluous-parens
             self.blink_eyes([0, 0, 0])
             break
 
@@ -79,58 +89,64 @@ class Robot:
         self.speech_service.setAudioExpression(True)
         self.speech_service.setVisualExpression(True)
 
-        return self.speech_to_text("speech.wav")
+        return self.speech_to_text_swe("speech.wav")
 
     def speech_to_text(self, audio_file):
+        """audio_file"""
         audio_file = speech_recognition.AudioFile("/tmp/" + audio_file)
         with audio_file as source:
             audio = self.recognizer.record(source)
-            # recognized = self.recognizer.recognize_google(audio, language="en_US")
             recognized = self.recognizer.recognize_google(audio, language="en_US")
-            print("[INFO]: s2t:" + recognized)
+            print("[INFO]: s2t:" + recognized)# pylint: disable=superfluous-parens
+        return recognized
+
+    def speech_to_text_swe(self, audio_file):# pylint: disable=no-self-use
+        """audio_file"""
+        url = 'http://92.32.44.72:5000/recieve'# server request endpoint
+        audio_file = {"file": open('/tmp/' + audio_file, 'rb')}
+        with open('/tmp/speech.wav', 'rb') as source:
+            requests.post(url, files={'fieldname': source})
+            request = requests.post(
+                url, files=audio_file)# Recieves the json with the transcibed text
+            recognized = request.text[11:-2]# budget json handling, the text
+            print("[INFO]: s2t:" + recognized)# pylint: disable=superfluous-parens
         return recognized
 
     def say(self, text):
-        #stuff
+        """text"""
         self.tts_service.say(text)
-        print("[INFO]: Robot says: " + text)
+        print("[INFO]: Robot says: " + text)# pylint: disable=superfluous-parens
 
     def set_awareness(self, state):
+        """state"""
         if state:
             self.awareness_service.resumeAwareness()
-            print("[INFO]: Awareness is turned on")
+            print("[INFO]: Awareness is turned on")# pylint: disable=superfluous-parens
         else:
             self.awareness_service.pauseAwareness()
-            print("[INFO]: Awareness is paused")
+            print("[INFO]: Awareness is paused")# pylint: disable=superfluous-parens
 
-    
+
     def ask_wikipedia(self):
-        #stuff
-        self.tablet_service.showImage("https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
+        """ask_wikipedia"""
+        self.tablet_service.showImage(
+            "https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
         time.sleep(1)
         self.speech_service.setAudioExpression(False)
         self.speech_service.setVisualExpression(False)
         self.set_awareness(False)
-        self.say("Give me a question wikipedia")
-        # self.say("vad vill du mig")
+        # self.say("Give me a question wikipedia")
+        self.say("vad vill du mig")
         question = self.listen()
-        self.say("I will tell you")
-        # self.say("jag har dig bror")
+        # self.say("I will tell you")
+        self.say("jag har dig bror")
         answer2 = tools.get_image_wikipedia(question)
-        #############TABLET IMAGE###############
-        print("[INFO]: answer2 (wikiimage): " + answer2)
         self.tablet_service.showImage(answer2)
-        #############SAY ANSWER##################
         answer = tools.get_knowledge_wikipedia(question)
         self.say(answer)
         self.set_awareness(True)
         self.speech_service.setAudioExpression(True)
         self.speech_service.setVisualExpression(True)
         time.sleep(2)
-        self.tablet_service.showImage("https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
-        
-
-
-#    def ask_google(self):
-
-
+        self.tablet_service.showImage(
+            "https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
--- tools.py
+++ tools.py
@@ -1,20 +1,19 @@
+"""
+Robot module
+"""
+
 import wikipedia
 
-
 def get_knowledge_wikipedia(term):
-    #stuff
-    # wikipedia.set_lang("sv")
-    # wikipedia.set_lang("en")
-    summary = wikipedia.summary(term, sentences=2)
+    """term"""
+    wikipedia.set_lang("sv")
+    summary = wikipedia.summary(term, sentences=1)
     return summary
 
 def get_image_wikipedia(term):
+    """term"""
+    wikipedia.set_lang("sv")
     page = wikipedia.page(term)
     wikiimage = page.images[0]
-    print("[INFO]: wikiimage: " + wikiimage)
+    print("[INFO]: wikiimage: " + wikiimage)# pylint: disable=superfluous-parens
     return wikiimage
-
-
-# def get_knowledge_google(term):
-    # Define buildargs for cse api
-
