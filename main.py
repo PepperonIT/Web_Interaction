13fc19698c3dddca9b83b0609a2decafde6f1608
oscarlundberg95 <oscarlundberg95@hotmail.se>
2022-11-03 15:57:56 +0000

    Swedish working, pylint implemented

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from PepperConnection import PepperConnection
 from listen import Listen
 import os
@@ -32,4 +33,22 @@ def main():
     #tts.say("Hello there")
     
 
+=======
+"""
+Main module
+"""
+from robot import Robot
+
+def main():
+    """infinite loop for testing"""
+    pepper = Robot("130.240.238.32", "9559")
+    while True:
+        try:
+            pepper.ask_wikipedia()
+            # pepper.ask_google()
+        except Exception as error:# pylint: disable=broad-except
+            print(error)# pylint: disable=superfluous-parens
+            # pepper.say("I dont know mate")
+            pepper.say("Vet inte fett oklart")
+>>>>>>> 224322b... Swedish working, pylint implemented
 main()
