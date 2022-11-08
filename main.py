47bce09626c0a130872b8f6cb334220b1e5f1566
oscarlundberg95 <oscarlundberg95@hotmail.se>
2022-11-08 16:56:07 +0000

    updated main

--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 Main module
 """
 from robot import Robot
+import config
 
 def main():
     """infinite loop for testing"""
-    pepper = Robot("ip", "port")
+    pepper = Robot(config.IP_ADDRESS, config.PORT)
     while True:
         try:
             pepper.ask_wikipedia()
         except Exception as error:# pylint: disable=broad-except
             print(error)# pylint: disable=superfluous-parens
-            # pepper.say("I dont know mate")
-            pepper.say("Vet inte fett oklart")
-
 main()
